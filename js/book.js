ddccebb6e1c9107a7537f418f4e3e5a5983abe90
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-09-03 18:24:59 +0600

    Search and Result section is complete

--- js/book.js
+++ js/book.js
@@ -1,1 +1,22 @@
-console.log('kaj kore');
+const loadBook =()=>{
+    const getInputData= document.getElementById('search-input');
+    const inputValue = getInputData.value;
+    getInputData.value = '';
+    const url = `http://openlibrary.org/search.json?q=${inputValue}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => searchResult(data))
+}
+const searchResult = bookinfo =>{
+    console.log(bookinfo);
+    const booksDetails = bookinfo.docs;
+    let bookNumber = bookinfo.numFound;
+    let searchBookNumber = document.getElementById('result-section');
+    searchBookNumber.innerHTML = `<h4>Total Search Result:${bookNumber}</h4>`
+    
+    booksDetails.forEach(details => {
+        console.log(details.title);
+        const bookDesign = document.getElementById('result-section');
+        
+    });
+}
