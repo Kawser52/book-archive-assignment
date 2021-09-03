36415661522b8c9bcaf51917039cc67fb94c96b0
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-09-03 19:51:21 +0600

    Motamoti complete

--- js/book.js
+++ js/book.js
@@ -1,29 +1,35 @@
 // fetch Data
+document.getElementById('error-message').style.display = 'none';
 const loadBook =()=>{
     const getInputData= document.getElementById('search-input');
     const inputValue = getInputData.value;
     getInputData.value = '';
+    document.getElementById('error-message').style.display = 'none';
+    if(inputValue === ''){
+        document.getElementById('error-message').style.display = 'block';
+    } else{
     const url = `http://openlibrary.org/search.json?q=${inputValue}`
     fetch(url)
     .then(res => res.json())
     .then(data => searchResult(data))
+    }
+   
 }
 
 // Search Result Section
 const searchResult = bookinfo =>{
-    console.log(bookinfo);
     const booksDetails = bookinfo.docs;
     let bookNumber = bookinfo.numFound;
     let searchBookNumber = document.getElementById('result-section');
     searchBookNumber.innerHTML = `<h4>Total Search Result:${bookNumber}</h4>`
-    
+     const clearData= document.getElementById('book-show');
+    clearData.textContent = '';
     booksDetails.forEach(details => {
-        console.log(details.title);
         const bookDesign = document.getElementById('book-show');
         const div = document.createElement('col-md-4');
         div.innerHTML = `
                 <div class="card mt-3" style="width: 18rem;">
-                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <img class="card-img-top" src="https://covers.openlibrary.org/b/id/${details.cover_i}-M.jpg"  class="img-fluid" alt="Card image cap" width ='100' height = '350';>
                     <div class="card-body">
                         <h5 class="card-title">${details.title}</h5>
                     </div>
@@ -39,3 +45,7 @@ const searchResult = bookinfo =>{
 
     });
 }
+
+function errorCheck(){
+
+};
